a91272a67b3fd6d7d0f0e1d575a94845361f1f35
y.chernyak <y.chernyak>
2024-06-07 13:00:44 +0300

    test

--- core/services/osm_services.py
+++ core/services/osm_services.py
@@ -134,16 +134,129 @@ def polygon_contains_point(coords: [], position: []):
     return polygon.contains(point)
 
 
+"""test """
+from scipy.spatial import ConvexHull
+import numpy
+
+def get_sorted_points(points, building_levels):
+    print('POINTS: ', points)
+    # Найдем выпуклую оболочку
+    points1 = numpy.array(points)
+    hull = ConvexHull(points1)
+    hull_indices = hull.vertices
+
+    # # Получим точки, входящие в выпуклую оболочку
+    sorted_points = [points[i] for i in hull_indices]
+    print('SORTED POINTS: ', sorted_points)
+    # return hull_points
+
+    hull_points = points1[hull.vertices]
+    # Вычислим площадь многоугольника
+    A = 0
+    Cx = 0
+    Cy = 0
+    n = len(hull_points)
+
+    for i in range(n):
+        x0, y0 = hull_points[i]
+        x1, y1 = hull_points[(i + 1) % n]
+        cross = (x0 * y1) - (x1 * y0)
+        A += cross
+        Cx += (x0 + x1) * cross
+        Cy += (y0 + y1) * cross
+
+    A /= 2
+    # кординаты центальной точки полигона
+    Cx /= (6 * A)
+    Cy /= (6 * A)
+    print("CENTRAL POINTS: ", [Cx, Cy])
+
+    # получаем линии. от центральной точки до каждой точки полигона
+    lines_to_extend = []
+    for point in sorted_points:
+        line = []
+        line.append([Cx, Cy])
+        line.append(point)
+        lines_to_extend.append(line)
+    print("LINES TO EXTEND: ", lines_to_extend)
+
+    # расширяем полученные линии на building_levels от центральной точки
+    result = []
+    for line in lines_to_extend:
+        new_line = []
+        bearing = calculate_initial_compass_bearing(line[0], line[1])
+        new_point = calculate_destination_point(line[1][0],
+                                                 line[1][1],
+                                                 bearing + 180,
+                                                 building_levels * 3)
+        result.append(new_point)
+        #new_line.append(line[0])
+        #new_line.append(new_point)
+        #result.append(new_line)
+    print("RESULT: ", result)
+
+    return result
+
+
 """ENTRY"""
 
+
+# def entry_point(lat, lon):
+#     cache_key = f"{str(lat)} {str(lon)}"
+#     cached_data = redis.get(cache_key)
+#     if cached_data is not None:
+#         result = json.loads(cached_data)
+#         return result
+#
+#     # Получаем ближайшие здания в радиусе от точки
+#     nearest_buildings = get_nearest_buildings(lat, lon)
+#
+#     buildings = []
+#     for build in nearest_buildings:
+#         build_elem = dict()
+#         if build['type'] == 'way':
+#             build_elem['id'] = build['id']
+#             build_elem['levels'] = 1
+#             if 'building:levels' in build['tags']:
+#                 build_elem['levels'] = int(build['tags']['building:levels'])
+#         if bool(build_elem):
+#             buildings.append(build_elem)
+#
+#     # Формируем ноды
+#     for build in buildings:
+#         build['nodes'] = get_way_nodes(build['id'])
+#         build['nodes_rect'] = get_rectangle_points(build['nodes'])
+#         lines_to_extend = get_min_lines(build['nodes_rect'])
+#         new_lines = []
+#         for line in lines_to_extend:
+#             bearing = calculate_initial_compass_bearing(line[0], line[1])
+#             # Calculate new points by extending the line
+#             new_point1 = calculate_destination_point(line[0][0],
+#                                                      line[0][1],
+#                                                      bearing + 180,
+#                                                      build['levels']*3)
+#             new_point2 = calculate_destination_point(line[1][0],
+#                                                      line[1][1],
+#                                                      bearing,
+#                                                      build['levels']*3)
+#             new_lines.append(new_point1)
+#             new_lines.append(new_point2)
+#         build['nodes_rect_ext'] = get_rectangle_points(new_lines)
+#         build['parking'] = not polygon_contains_point(build['nodes_rect_ext'], [lat, lon])
+#     # CACHE
+#     if len(buildings) > 0:
+#         json_data = json.dumps(buildings)
+#         redis.set(cache_key, json_data)
+#     return buildings
 
 def entry_point(lat, lon):
     cache_key = f"{str(lat)} {str(lon)}"
     cached_data = redis.get(cache_key)
     if cached_data is not None:
+        print("CACHE GET")
         result = json.loads(cached_data)
         return result
-
+    print("! cache")
     # Получаем ближайшие здания в радиусе от точки
     nearest_buildings = get_nearest_buildings(lat, lon)
 
@@ -164,25 +277,15 @@ def entry_point(lat, lon):
         build['nodes_rect'] = get_rectangle_points(build['nodes'])
         lines_to_extend = get_min_lines(build['nodes_rect'])
         new_lines = []
-        for line in lines_to_extend:
-            bearing = calculate_initial_compass_bearing(line[0], line[1])
-            # Calculate new points by extending the line
-            new_point1 = calculate_destination_point(line[0][0],
-                                                     line[0][1],
-                                                     bearing + 180,
-                                                     build['levels']*3)
-            new_point2 = calculate_destination_point(line[1][0],
-                                                     line[1][1],
-                                                     bearing,
-                                                     build['levels']*3)
-            new_lines.append(new_point1)
-            new_lines.append(new_point2)
-        build['nodes_rect_ext'] = get_rectangle_points(new_lines)
+
+        build['nodes_rect_ext'] = get_sorted_points(build['nodes'], build['levels']*3)
         build['parking'] = not polygon_contains_point(build['nodes_rect_ext'], [lat, lon])
     # CACHE
     if len(buildings) > 0:
+        print("cache set")
         json_data = json.dumps(buildings)
         redis.set(cache_key, json_data)
     return buildings
+
 
 
