e68cc79db727e10bbc8f57298b048cbc03ed9e6f
peewpaaw <80004255+peewpaaw@users.noreply.github.com>
2024-07-07 18:48:19 +0300

    extend polygon via extended lines, accident area, hiden tracked vehicles (#3)
    * test
    
    * test
    
    * test
    
    * extended all points in building via lines from points
    
    * accident area, tracked vehicles in .env file
    
    ---------
    
    Co-authored-by: y.chernyak <y.chernyak>

--- core/settings.py
+++ core/settings.py
@@ -6,6 +6,7 @@ load_dotenv()
 
 BTS_API_URL = os.getenv("BTS_API_URL")
 BTS_TOKEN = os.getenv("BTS_TOKEN")
+TRACKED_VEHICLES = os.getenv("TRACKED_VEHICLES").split(" ")
 
 REDIS_HOST = os.getenv("REDIS_HOST")
 redis = redis.Redis(host=REDIS_HOST, port=6379, db=0)
