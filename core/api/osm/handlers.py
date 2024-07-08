a42ed74d4e58790d40ffa0faaf92b66a2e0a8ef2
y.chernyak <y.chernyak>
2024-07-08 09:56:03 +0300

    fix accident area

--- core/api/osm/handlers.py
+++ core/api/osm/handlers.py
@@ -12,5 +12,5 @@ async def get_nearest(lat, lon):
 
 
 @osm_router.get("/accident_area")
-async def get_accident_area(object_id):
+async def accident_area(object_id):
     return get_accident_area(object_id)
